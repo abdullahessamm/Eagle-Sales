4c28dc351f3c7df175e064e0ce30e11fde6202e4
abdullahessamm <abdoessam.2010@gmail.com>
2022-09-26 13:40:28 +0200

    fix places routes issue (add middlewares)

--- routes/api/Places/index.php
+++ routes/api/Places/index.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AvailablePlacesController::class)->group(function () {
     Route::get('/', 'getPlaces');
-    Route::put('/add-country', 'addCountry');
-    Route::put('/add-city', 'addCity');
-    Route::patch('/update-country', 'updateCountry');
-    Route::patch('/update-city', 'updateCity');
-    Route::delete('/delete-country', 'deleteCountry');
-    Route::delete('/delete-city', 'deleteCity');
+    Route::put('/add-country', 'addCountry')->middleware(['auth', 'approved', 'admin']);
+    Route::put('/add-city', 'addCity')->middleware(['auth', 'approved', 'admin']);
+    Route::patch('/update-country', 'updateCountry')->middleware(['auth', 'approved', 'admin']);
+    Route::patch('/update-city', 'updateCity')->middleware(['auth', 'approved', 'admin']);
+    Route::delete('/delete-country', 'deleteCountry')->middleware(['auth', 'approved', 'admin']);
+    Route::delete('/delete-city', 'deleteCity')->middleware(['auth', 'approved', 'admin']);
 });
